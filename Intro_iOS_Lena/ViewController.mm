c79fd26058a50c21d5a4c917bf5dde8c20f3de53
Simon Lucey <slucey@new-host-3.home>
2015-09-07 11:02:23 -0400

    Made some small changes to ensure things are readable within Xcode and things hopefully work on the MAC clusters.

--- Intro_iOS_Lena/ViewController.mm
+++ Intro_iOS_Lena/ViewController.mm
@@ -14,7 +14,7 @@ using namespace std;
 
 @interface ViewController () {
     // Setup the view
-    UIImageView *OpenCV_View_;
+    UIImageView *imageView_;
 }
 @end
 
@@ -25,33 +25,15 @@ using namespace std;
     // Do any additional setup after loading the view, typically from a nib.
     
     // 1. Setup the your OpenCV view, so it takes up the entire App screen......
-    OpenCV_View_ = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)];
+    imageView_ = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height)];
     
     // 2. Important: add OpenCV_View as a subview
-    [self.view addSubview:OpenCV_View_];
+    [self.view addSubview:imageView_];
     
     // 3.Read in the image (of the famous Lena)
     UIImage *image = [UIImage imageNamed:@"lena.png"];
-    if(image != nil) OpenCV_View_.image = image; // Display the image if it is there....
+    if(image != nil) imageView_.image = image; // Display the image if it is there....
     else cout << "Cannot read in the file" << endl;
-    
-    // 4. (Optional) Read in numbers from a text file (using C++ syntax)
-    /*
-     const char* filename = "lena.png";
-     //NSString *numbersPath = [[NSBundle mainBundle] pathForResource:@"lena" ofType:@"png"];
-     //const char* filename = [numbersPath UTF8String];
-     
-     ifstream inFile(filename);
-     if(inFile.is_open()) {
-     unsigned char n = 0;
-     while(!inFile.eof()) {
-     inFile >> n;
-     cout << n << " ";
-     }
-     cout << endl;
-     }
-     else cout << "Cannot read in lena.png file" << endl;
-     */
     
     // ALL DONE :)
 }
